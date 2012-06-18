ee021a08a83170ea3b2b2c125330a0f4e14e602e
Carl Mackey <carl@animoto.com>
2012-06-18 14:34:50 -0400

    experiment listings and such

--- admin/admin.rb
+++ admin/admin.rb
@@ -14,7 +14,21 @@ $experimoto.start_syncing_thread
 
 require 'sinatra'
 
-
 get '/hi' do
   'Hello World!'
 end
+
+get '/experiment_list' do
+  ("<html><head></head><body>" +
+   $experimoto.experiments.keys.sort.join('<br />') +
+   "</body></html>"
+   )
+end
+
+get '/experiment_create' do
+  params.each do |k,v|
+    params[k.to_sym] = v
+  end
+  $experimoto.add_new_experiment(params)
+end
+
