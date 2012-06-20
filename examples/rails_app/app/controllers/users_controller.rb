f50da8de985979b9fa1cb43187566f8235125543
Carl Mackey <carl@animoto.com>
2012-06-20 14:33:00 -0400

    multivariate rails app user/new.html.erb

--- examples/rails_app/app/controllers/users_controller.rb
+++ examples/rails_app/app/controllers/users_controller.rb
@@ -45,6 +45,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         datum!('sign_up')
+        datum!('payment', @user.sub_cost)
         format.html { redirect_to @user, :notice => 'User was successfully created.' }
         format.json { render :json => @user, :status => :created, :location => @user }
       else
